f870e26fc7e18695e129d66b8bcec861e3991a6f
Nicholas Silas <njsilas@gmail.com>
2021-02-09 00:37:12 -0500

    commit tree 72bfc1b9624d07abe8e1bf1671e3e90c6b58ee2f

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+superbowlWin = (record) => {
+  let victory = record.find( record => record.result === "W" )
+  return !!victory ? victory.year : undefined
+}
